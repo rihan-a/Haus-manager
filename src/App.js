3f44e50186af10adeb5ccd4e5707ee759330987a
rihan-a <a.rihan@live.com>
2025-01-22 18:02:12 +0100

    import pages and add react router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Tasks from "./pages/Tasks";
+import ShoppingList from "./pages/ShoppingList";
+import Calendar from "./pages/Calendar";
+import Settings from "./pages/Settings";
+import Auth from "./pages/Auth";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tasks" element={<Tasks />} />
+        <Route path="/shopping-list" element={<ShoppingList />} />
+        <Route path="/calendar" element={<Calendar />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/auth" element={<Auth />} />
+      </Routes>
+    </Router>
+
+
   );
 }
 
